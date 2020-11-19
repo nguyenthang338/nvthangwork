197d3ce7c61b5ee9121cc9b09e7434070284c8cd
nguyenthang338 <nguyenthang33894@gmail.com>
2020-11-19 14:38:28 +0700

    modifile trading bot

--- tradingbot.js
+++ tradingbot.js
@@ -1,14 +1,22 @@
 let  Binance = require ('binance-api-node').default;
 const random = require('random');
 const jsonfile = require('jsonfile');
-const filedir = '/tmp/data.json'
+const filedir = './data.json'
 
 //jsonfile.writeFileSync(file, obj)
 // Authenticated client, can make signed calls
+const config = {
+  delay: 120000 , //Day la 2 phut dung khong nao.
+  percent: 0.4,
+  usdmin: 10,
+  binance: 'bot',
+}
 const client = Binance({
   apiKey: 'U6YoJUkYCHSOg0ekOt6KvqFNeJAE5eLBejFukBKgte2sv4NQvIjhNY7Rs3bHDlkz',
   apiSecret: '9LTKyEoUe4qtz4PPQ6l8fRatunaQincXH7N6M2a1QAXPj1RS3U3EQfrNHfKfW0Js' ,
 })
+
+
 
 async function buy (){
   let ArrayCoin = ['XMRUSDT','LTCUSDT','DOGEUSDT','XRPUSDT','BCHUSDT','EOSUSDT']
@@ -16,12 +24,13 @@ async function buy (){
   let number = random.int(min = 0, max = 5) // uniform integer in [ min, max ]
   let coinbuy = Arraycoin[number];
   let info = await client.accountInfo();
-  let usdt = info.balances.find(e => e ='USDT');
+  let usdt = info.balances.find(e => e.asset == 'USDT');
   let free = usdt.free;
 
   let lastprice = await client.prices({ symbol: coinbuy})
 
-  let quanity = (free / lastprice).toFixed(4) ;
+  if ( usdt > config.usdmin ){
+    let quanity = (free / lastprice).toFixed(4) ;
   //oder buy
   let result = await client.order({
     symbol: coinbuy,
@@ -31,6 +40,8 @@ async function buy (){
   }),
     //write  lai result to file
     jsonfile.writeFileSync(file, result)
+  }
+  console.log("Buy Symbol" + coinbuy + "\tLast price: " + lastprice + "Quantity:" + quanity);
 }
 
 //sell limit
@@ -39,43 +50,29 @@ async function buy (){
 async function sell () {
   let result = jsonfile.readFileSync(file);
   let lastprice = await client.prices({ symbol: result.symbol})
-  let percent = lastprice / file.price - 1;
+  let percent = 0 ;
+  if (lastprice > file.price ){
+     percent = lastprice / file.price - 1;
+  }else {
+    percent = 0;
+  }
   if (percent > config.percent ){
+    //Sell luon dung khong
     await client.order({
       symbol: result.symbol,
       side: 'SELL',
-      quantity: result.quantity,
+      quantity: result.origQty,
       price: lastprice,
     }),
   }
   result = {};
   //write file empty
   jsonfile.writeFileSync(file, result)
+  console.log("Sell Symbol "+ symbol + "Quantity" + result.origQty + "\tLast price: " + lastprice + "\t Oldprice:" + file.price + "\t Percent: " + percent );
+}
 
+function order {
+    buy () ;
+    sell () ;
 }
-async function order(){
-  console.log(
-    await client.order({
-      symbol: 'XLMETH',
-      side: 'BUY',
-      quantity: 100,
-      price: 0.0002,
-    }),
-  )
-}
-/*
-{
-  symbol: 'XLMETH',
-  orderId: 1740797,
-  clientOrderId: '1XZTVBTGS4K1e',
-  transactTime: 1514418413947,
-  price: '0.00020000',
-  origQty: '100.00000000',
-  executedQty: '0.00000000',
-  status: 'NEW',
-  timeInForce: 'GTC',
-  type: 'LIMIT',
-  side: 'BUY'
-}
-*/
-
+setInterval (order, config.delay );
